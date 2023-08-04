458763f5564bf192b88f041e39aac70ece409faa
Bathula Vamsi Krishna <121428816+vamsikrishna41@users.noreply.github.com>
2023-08-04 19:29:54 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,17 @@
+// import logo from './logo.svg';
+import './App.css';
+
+// import Score from './Components/Score';
+import Timer from './Components/Timer';
+// import TimerFunc from './Components/TimerFunc';
+
+function App() {
+  
+  return (
+    <div className="App">
+      <Timer />
+    </div>
+  );
+}
+
+export default App;
